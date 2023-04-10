b5d6bd2df23284269c20cedb700f9161b4ea8c76
dev-guy <devguy@devguy.com>
2023-04-09 22:09:44 -0700

    Fix esbuild version

--- config/config.exs
+++ config/config.exs
@@ -38,7 +38,7 @@ config :kantan, Kantan.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.16.17",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
