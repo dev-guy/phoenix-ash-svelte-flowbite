0034274532f155ccd887338a600c38ed234f38e2
Wout De Puysseleir <woutdp@gmail.com>
2023-04-12 19:44:30 -0700

    Upgrade LiveSvelte -> 0.4.2 (#5)

--- config/config.exs
+++ config/config.exs
@@ -66,9 +66,6 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Configure LiveSvelte
-config :live_svelte, :otp_name, :kantan
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
