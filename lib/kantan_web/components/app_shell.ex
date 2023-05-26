4691a541def372852abe03989c0a9a97e9e8bd97
dev-guy <devguy@devguy.com>
2023-05-26 09:58:22 -0700

    .

--- lib/kantan_web/components/app_shell.ex
+++ lib/kantan_web/components/app_shell.ex
@@ -766,7 +766,7 @@ defmodule KantanWeb.AppShell do
                   navigate={~p"/svelte/milkdown"}
                   class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 >
-                  Milkdown Editor
+                  Milkdown
                 </.link>
               </li>
               <li>
