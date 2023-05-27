f39c3108af51ed19d9d5752de41fae85195a917f
dev-guy <devguy@devguy.com>
2023-05-27 10:53:58 -0700

    Downgrade svelte

--- assets/svelte/milkdown/Hashes.svelte
+++ assets/svelte/milkdown/Hashes.svelte
@@ -1,19 +1,13 @@
 <script>
   // Source: https://stackblitz.com/edit/github-w7hjxh?file=README.md
   import { useWidgetViewContext } from '@prosemirror-adapter/svelte'
-  import { afterUpdate } from 'svelte';
-  let input = null;
-  afterUpdate(() => {
-    if (input) input.focus();
-	});
-
   const spec = useWidgetViewContext('spec')
   const hashes = Array(spec?.level || 0).fill('#').join('')
 </script>
 <span class="hash">{hashes}</span>
 <style>
   .hash {
-    color: red;
+    color: orange;
     margin-right: 6px;
   }
 </style>
