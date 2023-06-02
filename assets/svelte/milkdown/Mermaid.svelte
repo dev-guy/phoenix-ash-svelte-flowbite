73b429a9e6d127c46eb23b59e18c470461723fd5
dev-guy <devguy@devguy.com>
2023-06-02 08:51:20 -0700

    .

--- assets/svelte/milkdown/Mermaid.svelte
+++ assets/svelte/milkdown/Mermaid.svelte
@@ -1,11 +1,11 @@
 <script>
   import mermaid from "mermaid";
   import { useNodeViewContext } from "@prosemirror-adapter/svelte";
-  const contentRef = useNodeViewContext("contentRef");
   const node = useNodeViewContext("node");
   let diagram = $node.attrs.value
   let container;
 
+  // https://discuss.prosemirror.net/t/updating-node-attributes-after-rendering/2426 ?
 async function renderDiagram() {
   // https://stackoverflow.com/questions/7394748/whats-the-right-way-to-decode-a-string-that-has-special-html-entities-in-it?lq=1
   var txt = document.createElement("textarea");
