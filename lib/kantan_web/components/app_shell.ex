779a681f3d4de23f353b85b84de7783867632067
dev-guy <devguy@devguy.com>
2023-08-28 17:19:52 -0700

    Update dependencies

--- lib/kantan_web/components/app_shell.ex
+++ lib/kantan_web/components/app_shell.ex
@@ -813,7 +813,7 @@ defmodule KantanWeb.AppShell do
                 navigate={~p"/svelte/timeline"}
                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
               >
-                Accordion, tab groups, named slots
+                Skeleton Accordion, Skeleton tabs, Mermaid, Named slots, Skeleton and Flowbite Toast
               </.link>
             </li>
           </ul>
